02978f8d109dfe113344cd85302ac93372f0fdcb
zWolf-SW <zwolf@yandex.ru>
2023-03-14 02:33:21 +0300

    ign

--- web/index.php
+++ web/index.php
@@ -1,1 +1,6 @@
-<b>Yii2 - Video</b>
+<?php
+
+ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+    $config = require __DIR__ . '/../config/web.php';
+    $yii = new yii\web\Application($config);
+    $yii->run ();
